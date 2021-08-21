49d9124c6959fa3c63ea52469ed358da4c2779a9
nguyenductho89 <nguyenductho89@gmail.com>
2021-08-22 01:21:36 +0700

    add extension files

---
+++ CollectionPlaygrounds/Cosmetic/Model/CoreData/JSONEncoder+CoreData.swift
@@ -0,0 +1,18 @@
+//
+//  JSONEncoder+CoreData.swift
+//  Cosmetic
+//
+//  Created by Nguyễn Đức Thọ on 8/22/21.
+//
+
+import Foundation
+import CoreData
+
+extension JSONDecoder {
+    func decodeDataToManagedObject<T: DecoableNSManagedObject>(data:Data,
+                                                               managedContext: NSManagedObjectContext,
+                                                               type: T.Type) -> Result<T,Error> {
+        self.userInfo[CodingUserInfoKey.managedObjectContext] = managedContext
+        return Result { try self.decode(type, from: data) }
+    }
+}
